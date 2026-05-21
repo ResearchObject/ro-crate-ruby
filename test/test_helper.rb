ab890ee7687e127c58f8609ebffa12f2d0435d2a
Finn Bacall <finn.bacall@manchester.ac.uk>
2026-05-21 10:17:40 +0100

    Close open fixture files between tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,8 +5,18 @@ require 'test/unit'
 require 'ro_crate'
 require 'webmock/test_unit'
 
+def teardown
+  self._opened_files.each(&:close)
+end
+
+def _opened_files
+  @opened_files ||= []
+end
+
 def fixture_file(name, *args)
-  ::File.open(::File.join(fixture_dir, name), *args)
+  f = ::File.open(::File.join(fixture_dir, name), *args)
+  self._opened_files << f
+  f
 end
 
 def fixture_dir
